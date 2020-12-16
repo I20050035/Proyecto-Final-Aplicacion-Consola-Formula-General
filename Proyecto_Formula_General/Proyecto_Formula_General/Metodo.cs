bc925852e0b3640dc718b90fc5ee1b16a1ac4b9c
I20050035 <70992098+I20050035@users.noreply.github.com>
2020-12-15 23:18:16 -0600

    Add files via upload

---
+++ Proyecto_Formula_General/Proyecto_Formula_General/Metodo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Formula_General
+{
+    class Metodo
+    {
+        private int valorA;
+
+        public int ValorA
+        {
+            get
+            {
+                return valorA;
+            }
+            set
+            {
+                valorA = value;
+            }
+        }
+
+        private int valorB;
+
+        public int ValorB
+        {
+            get
+            {
+                return valorB;
+            }
+            set
+            {
+                valorB = value;
+            }
+        }
+
+        private int valorC;
+
+        public int ValorC
+        {
+            get
+            {
+                return valorC;
+            }
+            set
+            {
+                valorC = value;
+            }
+        }
+
+        private string resultado;
+
+        public string Resutado
+        {
+            get
+            {
+                return resultado;
+            }
+            set
+            {
+                resultado = value;
+            }
+        }
+
+        
+
+        //metodo
+        public string Formula(double a, double b, double c)
+        {
+            resultado = Convert.ToString(((-1 * b) + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a));
+            return resultado;
+        }
+    }
+}
---
+++ Proyecto_Formula_General/Proyecto_Formula_General/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Formula_General
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c; 
+            double x1 = 0, x2 = 0; 
+            Console.Write("Escribe el valor de a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Escribe el valor de b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Escribe el valor de c: ");
+            c = int.Parse(Console.ReadLine());
+             
+            x1 = ((-1 * b) + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);  
+            x2 = ((-1 * b) - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+
+            Console.Write("El valor de x1 es: {0}", x1);
+            Console.Write("El valor de x2 es: {0}", x2);
+            
+            Console.ReadKey(); 
+        }
+    }
+}
